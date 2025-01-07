bb66f69d409fa87d6d97cf895c2e8f501aff67ca
RynAgain <85427950+RynAgain@users.noreply.github.com>
2025-01-07 11:12:37 -0600

    Update MainScript.js

--- CAM_Tools/MainScript.js
+++ CAM_Tools/MainScript.js
@@ -6,10 +6,10 @@
 // @author       Ryan Satterfield
 // @match        https://*.cam.wfm.amazon.dev/*
 // @grant        none
-// @require      https://github.com/yourusername/Work_Productivity_Experiments/CAM_Tools/DownloadButton.js
-// @require      https://github.com/yourusername/Work_Productivity_Experiments/CAM_Tools/AddItemButton.js
-// @require      https://github.com/yourusername/Work_Productivity_Experiments/CAM_Tools/ActivateButton.js
-// @require      https://github.com/yourusername/Work_Productivity_Experiments/CAM_Tools/RedriveButton.js
+// @require      https://github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/DownloadButton.js
+// @require      https://github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/AddItemButton.js
+// @require      https://github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/ActivateButton.js
+// @require      https://github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/RedriveButton.js
 // ==/UserScript==
 
 (function() {
