31dc78497f58aff8bfe1c5a2a0c3557b8235ecda
RynAgain <85427950+RynAgain@users.noreply.github.com>
2025-01-15 09:35:08 -0600

    readding download

--- CAM_Tools/MainScript.js
+++ CAM_Tools/MainScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CAM_Admin_Tools
 // @namespace    http://tampermonkey.net/
-// @version      2.5.056
+// @version      2.5.057
 // @description  Main script to include button functionalities
 // @author       Ryan Satterfield
 // @match        https://*.cam.wfm.amazon.dev/*
@@ -16,6 +16,7 @@
 // @require      https://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@github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/NISFileToCAMUploadButton.js
 // @require      https://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@github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/ScanCodeTo13PLUButton.js
 // @require      https://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@github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/PLUToASINButton.js
+// @require      https://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@github.com/RynAgain/Work_Productivity_Experiments/raw/main/CAM_Tools/DownloadButton.js
 
 
 // @run-at       document-body
