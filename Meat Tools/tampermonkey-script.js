655d3d73aca309d2a681fe76b76974281707ca5a
Ryan Satterfield <85427950+RynAgain@users.noreply.github.com>
2025-01-16 13:19:55 -0600

    new tool

---
+++ Meat Tools/tampermonkey-script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Meat Team CAM Inventory Tool
+// @namespace    http://tampermonkey.net/
+// @version      0.0.001
+// @description  The ultimate Meat Team CAM Inventory Tool: Streamlining your meaty uploads with unparalleled precision and a dash of humor!
+// @author       Ryan Satterfield
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('Document fully loaded');
+        // Add your code to run after the document is fully loaded
+    });
+})();
