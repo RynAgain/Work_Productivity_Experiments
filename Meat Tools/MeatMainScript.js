a7081b5663d8125597b826deae5146329f216e53
Ryan Satterfield <85427950+RynAgain@users.noreply.github.com>
2025-02-11 16:26:40 -0600

    Update MeatMainScript.js

--- Meat Tools/MeatMainScript.js
+++ Meat Tools/MeatMainScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CAM_Admin_Tools - Meat Edition
 // @namespace    http://tampermonkey.net/
-// @version      2.5.133
+// @version      2.5.134
 // @description  Main script to include button functionalities
 // @author       Ryan Satterfield
 // @match        https://*.cam.wfm.amazon.dev/*
