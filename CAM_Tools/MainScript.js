359a8d24bfc59477fcfd4d7e4e79deeeb271e094
Ryan Satterfield <85427950+RynAgain@users.noreply.github.com>
2025-03-13 15:26:43 -0500

    Update MainScript.js

--- CAM_Tools/MainScript.js
+++ CAM_Tools/MainScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CAM_Admin_Tools
 // @namespace    http://tampermonkey.net/
-// @version      2.6.026
+// @version      2.6.027
 // @description  Main script to include button functionalities
 // @author       Ryan Satterfield
 // @match        https://*.cam.wfm.amazon.dev/*
