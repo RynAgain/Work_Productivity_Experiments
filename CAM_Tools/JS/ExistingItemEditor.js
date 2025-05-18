50ad41a7d785acbfe7947efe857b770f5b32433d
Ryan Satterfield <85427950+RynAgain@users.noreply.github.com>
2025-05-18 14:43:21 -0500

    111

--- CAM_Tools/JS/ExistingItemEditor.js
+++ CAM_Tools/JS/ExistingItemEditor.js
@@ -4,8 +4,8 @@
     // SVG icon for the quick tools menu
     const editorIcon = `
         <svg width="22" height="22" fill="none" stroke="#fff" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-            <polygon points="12,2 19,8 19,16 12,22 5,16 5,8" fill="#004E36" stroke="#fff" stroke-width="1.5"/>
-            <path d="M12 6v12M9 9l6 6M15 9l-6 6" stroke="#fff" stroke-width="1.5"/>
+            <path d="M12 19l7-7 3 3-7 7-3-3z" fill="#004E36" stroke="#fff" stroke-width="1.5"/>
+            <path d="M18 13l-6 6M2 12l6 6M3 3l18 18" stroke="#fff" stroke-width="1.5"/>
         </svg>
     `;
 
