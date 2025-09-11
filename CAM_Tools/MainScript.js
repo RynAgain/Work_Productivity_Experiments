9a92ba82c67e917edbe83ca648bde466fc58c17d
Ryan Satterfield <85427950+RynAgain@users.noreply.github.com>
2025-09-11 11:42:00 -0500

    Create MainScript.js

---
+++ CAM_Tools/MainScript.js
@@ -0,0 +1,2 @@
+//New URL please go to 	https://raw.githubusercontent.com/RynAgain/Work_Productivity_Experiments/main/CAM_Tools/MainScript.user.js
+// to install the fully updated version of the cam tools.  
